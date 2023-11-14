eadbbb4d4d4f45c1890db001fd1b8d71d2302460
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-11-14 16:00:39 +0400

    1.8-1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,27 +6,22 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const onGood = () => setGood(good + 1)
+  const onNeutral = () => setNeutral(neutral + 1)
+  const onBad = () => setBad(bad + 1)
 
-  const onGood = () => {
-    setGood(good + 1)
-  }
-
-  const onNeutral = () => {
-    setNeutral(neutral + 1)
-  }
-
-  const onBad = () => {
-    setBad(bad + 1)
-  }
+  let total = good + neutral + bad
 
   const Statistics = () => {
-    return (
+
+    if (good + neutral + bad === 0) return 'No feedback given'
+    else return (
       <>
         <p>Good: {good}</p>
         <p>Neutral: {neutral}</p>
         <p>Bad: {bad}</p>
-        <p>Total: {good + neutral + bad}</p>
-        <p>Average: {Math.floor((good + neutral + bad) * 100 / 3) / 100}</p>
+        <p>Total: {total}</p>
+        <p>Average: {Math.floor((good - bad) * 100 / total) / 100}</p>
         <p>Positive: {Math.floor(good / (good + neutral + bad) * 1000) / 10 + '%'}</p>
       </>
     )
