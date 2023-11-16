72b8d0965f275a52cdafb05436c0bc42e7f75635
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-11-16 14:14:51 +0400

    2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,34 @@
 import { useState } from 'react'
 
-const Numbers = ( { persons } ) => {
-  return persons.map(person => {
-    return <p key={person.name}>{person.name} {person.number}</p>
-  })
+let id = 4
+
+const Numbers = ( { persons, search } ) => {
+  if (search === '') {
+    return persons.map(person => {
+      return <p key={person.name}>{person.name} {person.number}</p>
+    })
+  }
+  else {
+    return persons.map(person => {
+      if (person.name.toLowerCase().includes(search.toLowerCase())) {
+        return <p key={person.name}>{person.name} {person.number}</p>
+      };
+    })
+  }
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: 3}
-  ]) 
+
+  const [persons, setPersons] = useState(([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])) 
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
 
   const handleSubmit = e => {
     e.preventDefault()
@@ -20,19 +37,31 @@ const App = () => {
   }
   const handleName = e => setNewName(e.target.value)
   const handleNumber = e => setNewNumber(e.target.value)
+  const handleSearch = e => setSearch(e.target.value)
+
+  console.log(search);
 
   const handleAdd = () => {
     const exists = (persons.some(person => person.name === newName))
-    !exists ? setPersons([...persons, {name: newName, number: newNumber}]) : alert(`${newName} is already added to phonebook`)
+    !exists ? setPersons([...persons, {name: newName, number: newNumber, id: id++}]) : alert(`${newName} is already added to phonebook`)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <p>filter shown with 
+
+        <input
+          onChange={handleSearch}
+          value={search}
+        />
+    
+      </p>
       <form 
         onSubmit={handleSubmit} 
        >
         <div>
+        <h2>add a new</h2>
           name: <input 
             onChange={handleName}
             value={newName}
@@ -54,6 +83,7 @@ const App = () => {
       <h2>Numbers</h2>
         <Numbers 
           persons={persons}
+          search={search}
         />
     </div>
   )
