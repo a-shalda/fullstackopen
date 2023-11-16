3053622526df33e0fcfbd448ff931587c28dd221
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-11-16 15:58:42 +0400

    2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Numbers } from './Numbers'
 import { Search } from './Search'
 import { Add } from './Add'
-
 
 let id = 5
 
 const App = () => {
 
-  const [persons, setPersons] = useState(([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])) 
+  const [persons, setPersons] = useState([]) 
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'notes')
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -68,7 +74,7 @@ const App = () => {
           persons={persons}
           search={search}
         />
-        
+
     </div>
   )
 }
