4070df3416d83ac673895412fea5e088ca7d73cd
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-11-19 07:01:07 +0400

    2.20

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,5 +1,62 @@
 import { useState, useEffect } from "react"
 
+const WeatherDetails = ({ weatherData, capital }) => {
+
+  if (weatherData !== '') {
+
+    console.log(weatherData)
+    const img_id = weatherData.weather[0].icon
+    const img_src = `https://openweathermap.org/img/wn/${img_id}@2x.png`
+
+    return (
+      <>
+        <h2>Weather in {capital}</h2>
+        <p>Temperature: {weatherData.main.temp} Celcius</p>
+        <img src={img_src}/>
+        <p>Wind: {weatherData.wind.speed} m/s</p>
+      </>
+    )
+  }
+}
+
+const Fetch = ( {lat, lon, countryView} ) => {
+
+  const [weatherData, setWeatherData] = useState('')
+  const api_key = import.meta.env.VITE_SOME_KEY;
+
+  //Making a weather call
+  useEffect(() => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${api_key}`)
+    .then(response => {
+      if (!response.ok) throw new Error ('Bad request')
+      else return response.json()
+    })
+    .then(response => {
+      setWeatherData(response)
+    })
+    .catch(err => console.log(err.message))
+    console.log('weather fetched')
+
+  }, [])
+
+  return (
+    <>
+      <h1>{countryView.name.common}</h1>
+      <p>Capital: {countryView.capital[0]}</p>
+      <p>Area: {countryView.area}</p>
+      <h2>Languages:</h2>
+      <ul>
+        {Object.values(countryView.languages).map(language => <li key={language}>{language}</li>)}
+      </ul>
+      <img src={countryView.flags.png} width='200'/>
+      <WeatherDetails
+        weatherData={weatherData}
+        capital={countryView.capital[0]}
+      />
+    </>
+  )
+}
+
 const ShowCountries = ( {country, countries, handleView} ) => {
 
   if (country !== '') {
@@ -14,30 +71,25 @@ const ShowCountries = ( {country, countries, handleView} ) => {
 
       const countryIndex = countries.map(item => item.name.common).indexOf(filteredCountries[0])
       const countryView = countries[countryIndex]
+      const lat = countryView.capitalInfo.latlng[0]
+      const lon = countryView.capitalInfo.latlng[1]
 
-      return (
-        <>
-          <h1>{countryView.name.common}</h1>
-          <p>Capital: {countryView.capital[0]}</p>
-          <p>Area: {countryView.area}</p>
-          <h2>Languages:</h2>
-          <ul>
-            {Object.values(countryView.languages).map(language => <li key={language}>{language}</li>)}
-          </ul>
-          <img src={countryView.flags.png} width='200'/>
-        </>
-      )
+      return <Fetch
+                lat={lat}
+                lon={lon}
+                countryView={countryView}
+              />
     }
     else return filteredCountries.map(item => {
       return (
-        <>
-          <p key={item}>{item} &nbsp;
+        <p key={item}>{item} &nbsp;
             <button 
               onClick={() => handleView(item)}
             >
               Show
             </button> 
-        </p></>)
+        </p>
+      )
     })
 
   }
@@ -50,6 +102,7 @@ const App = () => {
 
   const [country, setCountry] = useState('')
   const [countries, setCountries] = useState([])
+  const handleView = (item) => setCountry(item)
 
   useEffect(() => {
     fetch('https://studies.cs.helsinki.fi/restcountries/api/all')
@@ -63,9 +116,8 @@ const App = () => {
       }))
     })
     .catch(err => console.log(err.message))
+    console.log('fetched')
   }, [])
-
-  const handleView = (item) => setCountry(item)
 
   return (
     <>
@@ -85,6 +137,5 @@ const App = () => {
     </>
   )
 }
-
 
 export default App
